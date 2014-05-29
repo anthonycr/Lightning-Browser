81a04cac1f37bc91226793ead4ff87653f1ad6b6
n8fr8 <nathan@freitas.net>
2014-05-29 13:31:05 -0400

    setup for external modules and proguard to reduce size resulting APK is < 600kb

---
+++ setup-ant.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+target="android-19"
+
+for f in `find external/ -name project.properties`; do
+projectdir=`dirname $f`
+    echo "Updating ant setup in $projectdir:"
+    android update lib-project -p $projectdir -t $target
+done
+android update project -p . --subprojects -t $target --name Lightning
+
+cp libs/android-support-v4.jar external/netcipher/libnetcipher/libs/android-support-v4.jar
+
