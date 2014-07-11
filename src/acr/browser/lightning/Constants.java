b26efa0b48d00e13e4491109e4d54f5e1102b3de
Anthony Restaino <anthonyrestaino11@gmail.com>
2014-07-11 07:47:47 -0400

    Added input encoding parameter to Google search string

--- src/acr/browser/lightning/Constants.java
+++ src/acr/browser/lightning/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
 	public static final String MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; U; Android 4.2; en-us; Nexus 4 Build/JOP24G) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
 	public static final int API = android.os.Build.VERSION.SDK_INT;
 	public static final String YAHOO_SEARCH = "http://search.yahoo.com/search?p=";
-	public static final String GOOGLE_SEARCH = "https://www.google.com/search?client=lightning&q=";
+	public static final String GOOGLE_SEARCH = "https://www.google.com/search?client=lightning&ie=UTF-8&oe=UTF-8&q=";
 	public static final String BING_SEARCH = "http://www.bing.com/search?q=";
 	public static final String DUCK_SEARCH = "https://duckduckgo.com/?t=lightning&q=";
 	public static final String DUCK_LITE_SEARCH = "https://duckduckgo.com/lite/?t=lightning&q=";
