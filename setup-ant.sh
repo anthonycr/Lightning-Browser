c939e7e22501799859a9c4af9a1d3aceff01386c
Anthony Restaino <anthonyrestaino11@gmail.com>
2014-07-22 18:36:36 -0400

    Updated with lib addition

--- setup-ant.sh
+++ setup-ant.sh
@@ -14,3 +14,5 @@ for f in `find external/ -name project.properties`; do
     android update lib-project -p $projectdir -t $target
 done
 android update project -p . --subprojects -t $target --name Lightning
+
+cp libs/android-support-v4.jar external/netcipher/libnetcipher/libs/android-support-v4.jar
