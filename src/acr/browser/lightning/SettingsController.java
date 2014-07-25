d47d03b7aff90e18f5a08719cbb95f96690759c0
Miłosz Sieradzki <milosz+github@sieradzki.it>
2014-07-25 20:16:09 +0200

    Add missing 'private' and 'public' access' modifiers, fix warnings

--- src/acr/browser/lightning/SettingsController.java
+++ src/acr/browser/lightning/SettingsController.java
@@ -5,24 +5,24 @@ package acr.browser.lightning;
 
 public class SettingsController {
 
-	static boolean clearHistory = false;
+	private static boolean clearHistory;
 
 	/**
 	 * The purpose of this class is so that I can clear the dropdown history in the main activities if the user selects
 	 * to clear the history from the disk in advanced settings
 	 */
-	static void setClearHistory(boolean choice) {
+	public static void setClearHistory(boolean choice) {
 		clearHistory = choice;
 	}
 
 	/**
 	 * return the choice
 	 */
-	static boolean getClearHistory() {
+	public static boolean getClearHistory() {
 		if (clearHistory) {
 			clearHistory = false;
 			return true;
 		}
-		return clearHistory;
+		return false;
 	}
 }
