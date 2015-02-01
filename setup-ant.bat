675df18a7d840f8ee721bb74c71e22f705b389f7
Miłosz Sieradzki <milosz+github@sieradzki.it>
2015-02-01 18:55:32 +0100

    Port changes from setup-ant.sh to setup-ant.bat

--- setup-ant.bat
+++ setup-ant.bat
@@ -3,6 +3,16 @@ SETLOCAL enabledelayedexpansion
 
 SET "target=android-21"
 
+RMDIR external\appcompat /s /q
+XCOPY "%ANDROID_HOME%\extras\android\support\v7\appcompat" "external\appcompat\*" /s /e /y /q
+
+REM This library is already included by netcipher, but SHA1 of JARs differ
+DEL /f /q external\appcompat\libs\android-support-v4.jar
+
+RMDIR external\palette /s /q
+XCOPY "%ANDROID_HOME%\extras\android\support\v7\palette" "external\palette\*" /s /e /y /q
+MKDIR external\palette\src
+
 REM Update ant setup in project and all sub-projects
 SET "pattern=project.properties"
 FOR /R "./external/" %%# in (*.properties) DO (
