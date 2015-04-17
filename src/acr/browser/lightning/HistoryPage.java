4e5eac4d5b27a00bcda05cb13bf47f148424ce6d
Anthony Restaino <anthonyrestaino11@gmail.com>
2015-04-17 08:47:08 -0400

    Revert "Revert "Fixed bug in dark mode where search suggestions wouldn't show up. Sped up app startup by using singleton in BookmarkManager.""
    This reverts commit cc6d7c7aa977667ca70ad321c92f4ce5f55a7700.

--- src/acr/browser/lightning/HistoryPage.java
+++ src/acr/browser/lightning/HistoryPage.java
@@ -53,7 +53,7 @@ public class HistoryPage {
 	}
 
 	private static List<HistoryItem> getWebHistory(Context context) {
-		HistoryDatabase databaseHandler = HistoryDatabase.getInstance(context);
+		HistoryDatabase databaseHandler = HistoryDatabase.getInstance(context.getApplicationContext());
 		return databaseHandler.getLastHundredItems();
 	}
 }
