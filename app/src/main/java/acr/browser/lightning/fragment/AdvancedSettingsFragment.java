c65cccb25cd3571d1f0b56f9e264fab79880c5c9
Anthony Restaino <anthonyrestaino11@gmail.com>
2015-09-14 20:03:35 -0400

    Remove unnecessary ClickListener classes

--- app/src/main/java/acr/browser/lightning/fragment/AdvancedSettingsFragment.java
+++ app/src/main/java/acr/browser/lightning/fragment/AdvancedSettingsFragment.java
@@ -164,13 +164,7 @@ public class AdvancedSettingsFragment extends PreferenceFragment implements Pref
                 }
             }
         });
-        picker.setNeutralButton(getResources().getString(R.string.action_ok),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
+        picker.setNeutralButton(getResources().getString(R.string.action_ok), null);
         picker.show();
     }
 
@@ -187,13 +181,7 @@ public class AdvancedSettingsFragment extends PreferenceFragment implements Pref
                 textEncoding.setSummary(Constants.TEXT_ENCODINGS[which]);
             }
         });
-        picker.setNeutralButton(getResources().getString(R.string.action_ok),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
+        picker.setNeutralButton(getResources().getString(R.string.action_ok), null);
         picker.show();
     }
 
@@ -212,13 +200,7 @@ public class AdvancedSettingsFragment extends PreferenceFragment implements Pref
                 }
             }
         });
-        picker.setNeutralButton(getResources().getString(R.string.action_ok),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
+        picker.setNeutralButton(getResources().getString(R.string.action_ok), null);
         picker.show();
     }
 }
