330949a265b3539e9f960d1c532901966964d60f
Viacheslav Titov <viacheslavtitov90@gmail.com>
2017-02-27 01:14:53 +0200

    re #1707 - fixed mobitech search url query parameter

--- app/src/main/java/acr/browser/lightning/constant/Constants.java
+++ app/src/main/java/acr/browser/lightning/constant/Constants.java
@@ -34,10 +34,10 @@ public final class Constants {
     public static final String YANDEX_SEARCH = "https://yandex.ru/yandsearch?lr=21411&text=";
     public static final String MOBITECH_SEARCH = "https://api-client.mobitech-search.xyz/?p_key=%1$s&user_id=%2$s&showSearchBar=true&q=";
     public static final String MOBITECH_SEARCH_WITHOUT_USER_ID = "https://api-client.mobitech-search.xyz/?p_key=%1$s&showSearchBar=true&q=";
-    public static final String STARTPAGE_MOBITECH_SEARCH = "https://browser.mobitech-search.xyz/start?p_key=%1$s&user_id=%2$s";
+    public static final String STARTPAGE_MOBITECH_SEARCH = "https://browser.mobitech-search.xyz/start?p_key=%1$s&user_id=%2$s&q=";
 
     public static final String TRECKING_MOBITECH_INSTALLED_APP = "http://installs.mobitech.io/v1/tracking/install"; //POST
-    public static final String STARTPAGE_MOBITECH_SEARCH_WITHOUT_USER_ID = "https://browser.mobitech-search.xyz/start?p_key=%1$s";
+    public static final String STARTPAGE_MOBITECH_SEARCH_WITHOUT_USER_ID = "https://browser.mobitech-search.xyz/start?p_key=%1$s&q=";
 
 
     // Custom local page schemes
