6ca31c61cf02f1c56fcbe9d501a7f147d8bfcc5f
anthony restaino <anthonyrestaino11@gmail.com>
2017-05-07 23:32:41 -0400

    Add documentation for BookmarkUiModel

--- app/src/main/java/acr/browser/lightning/activity/BookmarkUiModel.java
+++ app/src/main/java/acr/browser/lightning/activity/BookmarkUiModel.java
@@ -14,14 +14,32 @@ public class BookmarkUiModel {
 
     @Nullable private String mCurrentFolder;
 
+    /**
+     * Sets the current folder that is being shown.
+     * Use null as the root folder.
+     *
+     * @param folder the current folder, null for root.
+     */
     public void setCurrentFolder(@Nullable String folder) {
         mCurrentFolder = folder;
     }
 
+    /**
+     * Determines if the current folder is
+     * the root folder.
+     *
+     * @return true if the current folder is
+     * the root, false otherwise.
+     */
     public boolean isRootFolder() {
         return mCurrentFolder == null;
     }
 
+    /**
+     * Gets the current folder that is being shown.
+     *
+     * @return the current folder, null for root.
+     */
     @Nullable
     public String getCurrentFolder() {
         return mCurrentFolder;
