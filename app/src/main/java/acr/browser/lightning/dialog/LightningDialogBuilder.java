8a66a3f1888c6729e63cf40a37b838669c91fed6
anthony restaino <anthonyrestaino11@gmail.com>
2017-06-04 18:52:56 -0400

    Cleaning up lint

--- app/src/main/java/acr/browser/lightning/dialog/LightningDialogBuilder.java
+++ app/src/main/java/acr/browser/lightning/dialog/LightningDialogBuilder.java
@@ -45,7 +45,7 @@ import acr.browser.lightning.utils.Utils;
  * Created by Stefano Pacifici on 02/09/15, based on Anthony C. Restaino's code.
  */
 public class LightningDialogBuilder {
-    public static final String TAG = "LightningDialogBuilder";
+    private static final String TAG = "LightningDialogBuilder";
 
     public enum NewTab {
         FOREGROUND,
