8697aa5d7d8c25bf395cbf763e16fc86f0fc018c
anthony restaino <anthonyrestaino11@gmail.com>
2017-07-11 22:42:02 -0400

    Javadoc for tabs view

--- app/src/main/java/acr/browser/lightning/browser/TabsView.java
+++ app/src/main/java/acr/browser/lightning/browser/TabsView.java
@@ -1,12 +1,31 @@
 package acr.browser.lightning.browser;
 
+/**
+ * The interface for communicating to the tab list view.
+ */
 public interface TabsView {
 
+    /**
+     * Called when a tab has been added.
+     */
     void tabAdded();
 
+    /**
+     * Called when a tab has been removed.
+     *
+     * @param position the position of the tab that has been removed.
+     */
     void tabRemoved(int position);
 
+    /**
+     * Called when a tab's metadata has been changed.
+     *
+     * @param position the position of the tab that has been changed.
+     */
     void tabChanged(int position);
 
+    /**
+     * Called when the tabs are completely initialized for the first time.
+     */
     void tabsInitialized();
 }
