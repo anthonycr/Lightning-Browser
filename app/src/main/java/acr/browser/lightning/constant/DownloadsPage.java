2540282ab5ee9f03abc057dc1d9a4bdff05d1b65
anthony restaino <anthonyrestaino11@gmail.com>
2017-09-14 22:39:28 -0400

    Converting download page to utilize mezzanine

--- app/src/main/java/acr/browser/lightning/constant/DownloadsPage.java
+++ app/src/main/java/acr/browser/lightning/constant/DownloadsPage.java
@@ -20,10 +20,10 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import acr.browser.lightning.R;
 import acr.browser.lightning.BrowserApp;
 import acr.browser.lightning.database.downloads.DownloadItem;
 import acr.browser.lightning.database.downloads.DownloadsModel;
+import acr.browser.lightning.html.download.DownloadPageBuilder;
 import acr.browser.lightning.preference.PreferenceManager;
 import acr.browser.lightning.utils.Preconditions;
 import acr.browser.lightning.utils.Utils;
@@ -34,30 +34,6 @@ public final class DownloadsPage {
 
     public static final String FILENAME = "downloads.html";
 
-    private static final String HEADING_1 = "<!DOCTYPE html><html xmlns=http://www.w3.org/1999/xhtml>\n" +
-        "<head>\n" +
-        "<meta content=en-us http-equiv=Content-Language />\n" +
-        "<meta content='text/html; charset=utf-8' http-equiv=Content-Type />\n" +
-        "<meta name=viewport content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>\n" +
-        "<title>";
-
-    private static final String HEADING_2 = "</title></head><style>body,html {margin: 0px; padding: 0px;}" +
-        ".box { vertical-align:middle;position:relative; display: block; margin: 0px;padding-left:14px;padding-right:14px;padding-top:9px;padding-bottom:9px; background-color:#fff;border-bottom: 1px solid #d2d2d2;font-family: Arial;color: #444;font-size: 12px;}" +
-        ".box a { width: 100%; height: 100%; position: absolute; left: 0; top: 0;}" +
-        ".black {color: black;font-size: 15px;font-family: Arial; white-space: nowrap; overflow: hidden;margin:auto; text-overflow: ellipsis; -o-text-overflow: ellipsis; -ms-text-overflow: ellipsis;}" +
-        ".font {color: gray;font-size: 10px;font-family: Arial; white-space: nowrap; overflow: hidden;margin:auto; text-overflow: ellipsis; -o-text-overflow: ellipsis; -ms-text-overflow: ellipsis;}" +
-        "</style><body><div id=\"content\">";
-
-    private static final String PART1 = "<div class=box><a href='";
-
-    private static final String PART2 = "'></a><p class='black'>";
-
-    private static final String PART3 = "</p><p class='font'>";
-
-    private static final String PART4 = "</p></div>";
-
-    private static final String END = "</div></body></html>";
-
     @NonNull
     private static File getDownloadsPageFile(@NonNull Application application) {
         return new File(application.getFilesDir(), FILENAME);
@@ -67,11 +43,8 @@ public final class DownloadsPage {
     @Inject PreferenceManager mPreferenceManager;
     @Inject DownloadsModel mManager;
 
-    @NonNull private final String mTitle;
-
     public DownloadsPage() {
         BrowserApp.getAppComponent().inject(this);
-        mTitle = mApp.getString(R.string.action_downloads);
     }
 
     @NonNull
@@ -97,34 +70,14 @@ public final class DownloadsPage {
                     Preconditions.checkNonNull(list);
                     String directory = mPreferenceManager.getDownloadDirectory();
 
-                    final StringBuilder downloadsBuilder = new StringBuilder(HEADING_1 + mTitle + HEADING_2);
+                    DownloadPageBuilder downloadPageBuilder = new DownloadPageBuilder(mApp, directory);
 
-                    for (int n = 0, size = list.size(); n < size; n++) {
-                        final DownloadItem item = list.get(n);
-                        downloadsBuilder.append(PART1);
-                        downloadsBuilder.append("file://");
-                        downloadsBuilder.append(directory);
-                        downloadsBuilder.append("/");
-                        downloadsBuilder.append(item.getTitle());
-                        downloadsBuilder.append(PART2);
-                        downloadsBuilder.append(item.getTitle());
 
-                        if (!item.getContentSize().isEmpty()) {
-                            downloadsBuilder.append(" [");
-                            downloadsBuilder.append(item.getContentSize());
-                            downloadsBuilder.append("]");
-                        }
-
-                        downloadsBuilder.append(PART3);
-                        downloadsBuilder.append(item.getUrl());
-                        downloadsBuilder.append(PART4);
-                    }
-                    downloadsBuilder.append(END);
                     FileWriter bookWriter = null;
                     try {
                         //noinspection IOResourceOpenedButNotSafelyClosed
                         bookWriter = new FileWriter(getDownloadsPageFile(mApp), false);
-                        bookWriter.write(downloadsBuilder.toString());
+                        bookWriter.write(downloadPageBuilder.buildPage(list));
                     } catch (IOException e) {
                         Log.e(TAG, "Unable to write download page to disk", e);
                     } finally {
