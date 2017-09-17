f49a288783f7eb4bde7e1fe3f0bd82da981f7b4c
anthony restaino <anthonyrestaino11@gmail.com>
2017-09-16 23:20:10 -0400

    Fixing search view styling

--- app/src/main/java/acr/browser/lightning/view/SearchView.kt
+++ app/src/main/java/acr/browser/lightning/view/SearchView.kt
@@ -1,6 +1,7 @@
 package acr.browser.lightning.view
 
 import android.content.Context
+import android.support.v7.appcompat.R
 import android.support.v7.widget.AppCompatAutoCompleteTextView
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -9,24 +10,16 @@ import android.view.ViewConfiguration
 class SearchView @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
-) : AppCompatAutoCompleteTextView(
-        context,
-        attrs,
-        defStyleAttr
-) {
+        defStyleAttr: Int = R.attr.autoCompleteTextViewStyle
+) : AppCompatAutoCompleteTextView(context, attrs, defStyleAttr) {
 
     interface PreFocusListener {
         fun onPreFocus()
     }
 
-    private var listener: PreFocusListener? = null
+    var onPreFocusListener: PreFocusListener? = null
     private var isBeingClicked: Boolean = false
     private var timePressed: Long = 0
-
-    fun setOnPreFocusListener(listener: PreFocusListener?) {
-        this.listener = listener
-    }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
@@ -36,9 +29,7 @@ class SearchView @JvmOverloads constructor(
             }
             MotionEvent.ACTION_CANCEL -> isBeingClicked = false
             MotionEvent.ACTION_UP -> if (isBeingClicked && !isLongPress) {
-                if (listener != null) {
-                    listener!!.onPreFocus()
-                }
+                onPreFocusListener?.onPreFocus()
             }
         }
         return super.onTouchEvent(event)
