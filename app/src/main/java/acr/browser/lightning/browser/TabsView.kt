bb8d373f6f85c3245699ceecab04ed5d0488dd64
anthony restaino <anthonyrestaino11@gmail.com>
2017-09-18 20:55:15 -0400

    Converting TabsView to kotlin

--- app/src/main/java/acr/browser/lightning/browser/TabsView.kt
+++ app/src/main/java/acr/browser/lightning/browser/TabsView.kt
@@ -1,31 +1,31 @@
-package acr.browser.lightning.browser;
+package acr.browser.lightning.browser
 
 /**
  * The interface for communicating to the tab list view.
  */
-public interface TabsView {
+interface TabsView {
 
     /**
      * Called when a tab has been added.
      */
-    void tabAdded();
+    fun tabAdded()
 
     /**
      * Called when a tab has been removed.
      *
      * @param position the position of the tab that has been removed.
      */
-    void tabRemoved(int position);
+    fun tabRemoved(position: Int)
 
     /**
      * Called when a tab's metadata has been changed.
      *
      * @param position the position of the tab that has been changed.
      */
-    void tabChanged(int position);
+    fun tabChanged(position: Int)
 
     /**
      * Called when the tabs are completely initialized for the first time.
      */
-    void tabsInitialized();
+    fun tabsInitialized()
 }
