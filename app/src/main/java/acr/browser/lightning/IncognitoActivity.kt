82149d568e4dd5cc0ec7cc4d08390fc6ec5dd2b5
Anthony Restaino <anthonyrestaino11@gmail.com>
2017-09-20 20:57:40 -0400

    Converting top level activities to kotlin

---
+++ app/src/main/java/acr/browser/lightning/IncognitoActivity.kt
@@ -0,0 +1,49 @@
+package acr.browser.lightning
+
+import acr.browser.lightning.browser.activity.BrowserActivity
+import android.content.Intent
+import android.os.Build
+import android.view.Menu
+import android.webkit.CookieManager
+import android.webkit.CookieSyncManager
+import com.anthonycr.bonsai.Completable
+
+class IncognitoActivity : BrowserActivity() {
+
+    @Suppress("DEPRECATION")
+    public override fun updateCookiePreference(): Completable {
+        return Completable.create { subscriber ->
+            val cookieManager = CookieManager.getInstance()
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                CookieSyncManager.createInstance(this@IncognitoActivity)
+            }
+            cookieManager.setAcceptCookie(mPreferences.incognitoCookiesEnabled)
+            subscriber.onComplete()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.incognito, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        // handleNewIntent(intent);
+        super.onNewIntent(intent)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // saveOpenTabs();
+    }
+
+    override fun updateHistory(title: String?, url: String) {
+        // addItemToHistory(title, url);
+    }
+
+    public override fun isIncognito(): Boolean = true
+
+    override fun closeActivity() {
+        closeDrawers { closeBrowser() }
+    }
+}
---
+++ app/src/main/java/acr/browser/lightning/MainActivity.kt
@@ -0,0 +1,74 @@
+package acr.browser.lightning
+
+import acr.browser.lightning.browser.activity.BrowserActivity
+import android.content.Intent
+import android.os.Build
+import android.view.KeyEvent
+import android.view.Menu
+import android.webkit.CookieManager
+import android.webkit.CookieSyncManager
+import com.anthonycr.bonsai.Completable
+
+class MainActivity : BrowserActivity() {
+
+    @Suppress("DEPRECATION")
+    public override fun updateCookiePreference(): Completable {
+        return Completable.create { subscriber ->
+            val cookieManager = CookieManager.getInstance()
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                CookieSyncManager.createInstance(this@MainActivity)
+            }
+            cookieManager.setAcceptCookie(mPreferences.cookiesEnabled)
+            subscriber.onComplete()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        if (BrowserActivity.isPanicTrigger(intent)) {
+            panicClean()
+        } else {
+            handleNewIntent(intent)
+            super.onNewIntent(intent)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveOpenTabs()
+    }
+
+    override fun updateHistory(title: String?, url: String) {
+        addItemToHistory(title, url)
+    }
+
+    public override fun isIncognito(): Boolean = false
+
+    override fun closeActivity() {
+        closeDrawers {
+            performExitCleanUp()
+            moveTaskToBack(true)
+        }
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN && event.isCtrlPressed) {
+            when (event.keyCode) {
+                KeyEvent.KEYCODE_P ->
+                    // Open a new private window
+                    if (event.isShiftPressed) {
+                        startActivity(Intent(this, IncognitoActivity::class.java))
+                        overridePendingTransition(R.anim.slide_up_in, R.anim.fade_out_scale)
+                        return true
+                    }
+            }
+        }
+        return super.dispatchKeyEvent(event)
+    }
+
+
+}
