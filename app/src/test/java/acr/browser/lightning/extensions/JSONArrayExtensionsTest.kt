f5d14707988588287d5858a2d7805eef62f96fd0
Anthony Restaino <anthonyrestaino11@gmail.com>
2017-11-09 19:24:17 -0500

    Unit tests for JSONArrayExtensions

---
+++ app/src/test/java/acr/browser/lightning/extensions/JSONArrayExtensionsTest.kt
@@ -0,0 +1,27 @@
+package acr.browser.lightning.extensions
+
+import acr.browser.lightning.BuildConfig
+import acr.browser.lightning.SDK_VERSION
+import acr.browser.lightning.TestApplication
+import org.assertj.core.api.Assertions.assertThat
+import org.json.JSONArray
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+/**
+ * Unit tests for JSONArrayExtensions.kt
+ */
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, application = TestApplication::class, sdk = intArrayOf(SDK_VERSION))
+class JSONArrayExtensionsTest {
+
+    @Test
+    fun `map functions correctly`() {
+        val numberList = listOf(1, 2, 3, 4, 5, 6, 7)
+        val mockJsonArray = JSONArray(numberList)
+
+        assertThat(numberList).isEqualTo(mockJsonArray.map { it as Int })
+    }
+}
