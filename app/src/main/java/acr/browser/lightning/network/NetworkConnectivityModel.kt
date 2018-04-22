58887e28161e58345f3dbb0cd1eeceaf384b750f
Anthony Restaino <anthonyrestaino11@gmail.com>
2018-04-22 17:15:14 -0400

    Convert NetworkConnectivityModle to use ObservableBroadcastReceiver

--- app/src/main/java/acr/browser/lightning/network/NetworkConnectivityModel.kt
+++ app/src/main/java/acr/browser/lightning/network/NetworkConnectivityModel.kt
@@ -1,11 +1,8 @@
 package acr.browser.lightning.network
 
-import acr.browser.lightning.rx.DisposableBroadcastReceiver
+import acr.browser.lightning.rx.ObservableBroadcastReceiver
 import android.app.Application
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.net.ConnectivityManager
 import io.reactivex.Observable
 import javax.inject.Inject
@@ -22,21 +19,10 @@ class NetworkConnectivityModel @Inject constructor(private val application: Appl
      * An infinite observable that emits a boolean value whenever the network condition changes.
      * Emitted value is true when the network is in the connected state, and it is false otherwise.
      */
-    fun connectivity(): Observable<Boolean> = Observable.create {
-        val receiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                if (intent.action == NETWORK_BROADCAST_ACTION) {
-                    it.onNext(connectivityManager.activeNetworkInfo?.isConnected == true)
-                }
-            }
-        }
-
-        it.setDisposable(DisposableBroadcastReceiver(application, receiver))
-
-        application.registerReceiver(receiver, IntentFilter().apply {
-            addAction(NETWORK_BROADCAST_ACTION)
-        })
-    }
+    fun connectivity(): Observable<Boolean> = ObservableBroadcastReceiver(
+            NETWORK_BROADCAST_ACTION,
+            application
+    ).map { connectivityManager.activeNetworkInfo?.isConnected == true }
 
     companion object {
         private const val NETWORK_BROADCAST_ACTION = "android.net.conn.CONNECTIVITY_CHANGE"
---
+++ app/src/main/java/acr/browser/lightning/rx/ObservableBroadcastReceiver.kt
@@ -0,0 +1,37 @@
+package acr.browser.lightning.rx
+
+import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import io.reactivex.Observable
+import io.reactivex.Observer
+
+/**
+ * An [Observable] of [Intent] emitted from a [BroadcastReceiver] registered with the [Application].
+ *
+ * Created by anthonycr on 3/30/18.
+ */
+class ObservableBroadcastReceiver(
+        private val action: String,
+        private val application: Application
+) : Observable<Intent>() {
+
+    override fun subscribeActual(observer: Observer<in Intent>) {
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if (intent.action == action) {
+                    observer.onNext(intent)
+                }
+            }
+        }
+
+        application.registerReceiver(receiver, IntentFilter().apply {
+            addAction(action)
+        })
+
+        observer.onSubscribe(DisposableBroadcastReceiver(application, receiver))
+    }
+
+}
