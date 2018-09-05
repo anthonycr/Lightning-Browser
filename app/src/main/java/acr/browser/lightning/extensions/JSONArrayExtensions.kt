118ef723744f44f54589bc9957922cdd6309313c
Anthony Restaino <anthonyrestaino11@gmail.com>
2018-09-05 07:52:39 -0400

    Make JSONArray extension inline

--- app/src/main/java/acr/browser/lightning/extensions/JSONArrayExtensions.kt
+++ app/src/main/java/acr/browser/lightning/extensions/JSONArrayExtensions.kt
@@ -5,4 +5,4 @@ import org.json.JSONArray
 /**
  * Map each item in a [JSONArray] to a list of a new type.
  */
-fun <T> JSONArray.map(map: (Any) -> T): List<T> = (0 until length()).map { map(this[it]) }
+inline fun <T> JSONArray.map(map: (Any) -> T): List<T> = (0 until length()).map { map(this[it]) }
