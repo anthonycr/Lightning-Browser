53d1f48e75d7cdf0c29ed1a074e34fd6491b037a
Anthony Restaino <anthonyrestaino11@gmail.com>
2018-09-15 14:50:36 -0400

    Tests for BaiduSuggestionsModel

---
+++ app/src/test/java/acr/browser/lightning/search/suggestions/BaiduSuggestionsModelTest.kt
@@ -0,0 +1,36 @@
+package acr.browser.lightning.search.suggestions
+
+import acr.browser.lightning.unimplemented
+import android.app.Application
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+/**
+ * Unit tests for [BaiduSuggestionsModel].
+ */
+class BaiduSuggestionsModelTest {
+
+    private val httpClient = OkHttpClient.Builder().build()
+    private val requestFactory = object : RequestFactory {
+        override fun createSuggestionsRequest(httpUrl: HttpUrl, encoding: String) = unimplemented()
+    }
+    private val application = mock<Application> {
+        on { getString(any()) } doReturn "test"
+    }
+
+    @Test
+    fun `verify query url`() {
+        val suggestionsModel = BaiduSuggestionsModel(httpClient, requestFactory, application)
+
+        (0..100).forEach {
+            val result = "http://suggestion.baidu.com/s?wd=$it&action=opensearch"
+
+            assertThat(suggestionsModel.createQueryUrl(it.toString(), "null")).isEqualTo(HttpUrl.parse(result))
+        }
+    }
+}
