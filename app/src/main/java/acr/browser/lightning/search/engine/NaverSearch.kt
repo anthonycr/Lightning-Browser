e76373042f958584f294dee1250d2f9d6ee075fa
Anthony Restaino <anthonyrestaino11@gmail.com>
2018-09-14 22:52:38 -0400

    Updating NaverSearch to fit the new system

--- app/src/main/java/acr/browser/lightning/search/engine/NaverSearch.kt
+++ app/src/main/java/acr/browser/lightning/search/engine/NaverSearch.kt
@@ -8,7 +8,7 @@ import acr.browser.lightning.R
  * See https://en.m.wikipedia.org/wiki/File:Naver_Logotype.svg for the icon.
  */
 class NaverSearch : BaseSearchEngine(
-        "file:///android_asset/naver.png",
-        "https://search.naver.com/search.naver?ie=utf8&query=",
-        R.string.search_engine_naver
+    "file:///android_asset/naver.png",
+    "https://search.naver.com/search.naver?ie=utf8&query=",
+    R.string.search_engine_naver
 )
--- app/src/main/java/acr/browser/lightning/search/suggestions/NaverSuggestionsModel.kt
+++ app/src/main/java/acr/browser/lightning/search/suggestions/NaverSuggestionsModel.kt
@@ -4,12 +4,11 @@ import acr.browser.lightning.R
 import acr.browser.lightning.constant.UTF8
 import acr.browser.lightning.database.HistoryItem
 import acr.browser.lightning.extensions.map
-import acr.browser.lightning.search.engine.NaverSearch
-import acr.browser.lightning.utils.FileUtils
 import android.app.Application
+import okhttp3.HttpUrl
+import okhttp3.ResponseBody
 import org.json.JSONArray
 import org.json.JSONObject
-import java.io.InputStream
 
 /**
  * The search suggestions provider for the Naver search engine.
@@ -18,23 +17,33 @@ class NaverSuggestionsModel(application: Application) : BaseSuggestionsModel(app
 
     private val searchSubtitle = application.getString(R.string.suggestion)
 
-    override fun createQueryUrl(query: String, language: String): String =
-            "https://ac.search.naver.com/nx/ac?q=$query&q_enc=UTF-8&st=100&frm=nv&r_format=json&r_enc=UTF-8&r_unicode=0&t_koreng=1&ans=2&run=2&rev=4&con=1"
+    // https://ac.search.naver.com/nx/ac?q=$query&q_enc=UTF-8&st=100&frm=nv&r_format=json&r_enc=UTF-8&r_unicode=0&t_koreng=1&ans=2&run=2&rev=4&con=1
+    override fun createQueryUrl(query: String, language: String): HttpUrl =
+        HttpUrl.Builder()
+            .scheme("https")
+            .host("ac.search.naver.com")
+            .encodedPath("/nx/ac")
+            .addQueryParameter("q", query)
+            .addQueryParameter("q_enc", "UTF-8")
+            .addQueryParameter("st", "100")
+            .addQueryParameter("frm", "nv")
+            .addQueryParameter("r_format", "json")
+            .addQueryParameter("r_enc", "UTF-8")
+            .addQueryParameter("r_unicode", "0")
+            .addQueryParameter("t_koreng", "1")
+            .addQueryParameter("ans", "2")
+            .addQueryParameter("run", "2")
+            .addQueryParameter("rev", "4")
+            .addQueryParameter("con", "1")
+            .build()
 
-    @Throws(Exception::class)
-    override fun parseResults(inputStream: InputStream): List<HistoryItem> {
-        val content = FileUtils.readStringFromStream(inputStream, UTF8)
-        val jsonobj = JSONObject(content)
-
-        return jsonobj.getJSONArray("items")
-                .getJSONArray(0)
-                .map { it as JSONArray }
-                .map { it.get(0) as String }
-                .map { HistoryItem(
-                        "${NaverSearch().queryUrl}$it",
-                        it,
-                        R.drawable.ic_search)
-                }
+    override fun parseResults(responseBody: ResponseBody): List<HistoryItem> {
+        return JSONObject(responseBody.string())
+            .getJSONArray("items")
+            .getJSONArray(0)
+            .map { it as JSONArray }
+            .map { it[0] as String }
+            .map { HistoryItem("$searchSubtitle \"$it\"", it, R.drawable.ic_search) }
     }
 
 }
