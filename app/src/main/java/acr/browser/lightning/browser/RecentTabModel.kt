75e96ea4aeb618d962e798edbe3ffbc276c3f4fb
Anthony Restaino <anthonyrestaino11@gmail.com>
2018-10-20 19:23:01 -0400

    Add missing documentation

--- app/src/main/java/acr/browser/lightning/browser/RecentTabModel.kt
+++ app/src/main/java/acr/browser/lightning/browser/RecentTabModel.kt
@@ -10,8 +10,15 @@ class RecentTabModel {
 
     private val bundleQueue: Queue<Bundle> = ArrayDeque<Bundle>()
 
+    /**
+     * Return the last closed tab as a [Bundle] or null if there is no previously opened tab.
+     * Removes the [Bundle] from the queue after returning it.
+     */
     fun lastClosed(): Bundle? = bundleQueue.poll()
 
+    /**
+     * Add the [savedBundle] to the queue. The next call to [lastClosed] will return this [Bundle].
+     */
     fun addClosedTab(savedBundle: Bundle) = bundleQueue.add(savedBundle)
 
 }
