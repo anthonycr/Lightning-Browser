7e10429a8f34658164f2dcca0dd4a2d6a5ece99e
Anthony Restaino <anthonyrestaino11@gmail.com>
2018-11-05 20:37:40 -0500

    Create extension function that only calls pop if the stack is not empty

--- app/src/main/java/acr/browser/lightning/browser/RecentTabModel.kt
+++ app/src/main/java/acr/browser/lightning/browser/RecentTabModel.kt
@@ -1,5 +1,6 @@
 package acr.browser.lightning.browser
 
+import acr.browser.lightning.extensions.popIfNotEmpty
 import android.os.Bundle
 import java.util.*
 
@@ -14,7 +15,7 @@ class RecentTabModel {
      * Return the last closed tab as a [Bundle] or null if there is no previously opened tab.
      * Removes the [Bundle] from the queue after returning it.
      */
-    fun lastClosed(): Bundle? = bundleStack.pop()
+    fun lastClosed(): Bundle? = bundleStack.popIfNotEmpty()
 
     /**
      * Add the [savedBundle] to the queue. The next call to [lastClosed] will return this [Bundle].
