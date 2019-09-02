f304e646f696704104c53ecba50650a76ce3b47d
Anthony Restaino <anthonyrestaino11@gmail.com>
2019-09-02 18:00:16 -0400

    Creating an enum preference delegate

---
+++ app/src/main/java/acr/browser/lightning/preference/IntEnum.kt
@@ -0,0 +1,12 @@
+package acr.browser.lightning.preference
+
+/**
+ * An enum value that has an integer representation.
+ */
+interface IntEnum {
+
+    /**
+     * The integer representation of the enum value.
+     */
+    val value: Int
+}
