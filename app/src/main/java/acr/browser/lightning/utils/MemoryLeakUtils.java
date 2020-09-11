56b0fd0a7adbc13d4c60048946103399d2e77578
Anthony Restaino <anthonyrestaino11@gmail.com>
2020-09-10 22:56:55 -0400

    Fix lint warnings

--- app/src/main/java/acr/browser/lightning/utils/MemoryLeakUtils.java
+++ app/src/main/java/acr/browser/lightning/utils/MemoryLeakUtils.java
@@ -5,14 +5,15 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public final class MemoryLeakUtils {
 
@@ -73,27 +74,28 @@ public final class MemoryLeakUtils {
 
     }
 
+    @SuppressWarnings("NoopMethodInAbstractClass")
     public static abstract class LifecycleAdapter implements Application.ActivityLifecycleCallbacks {
         @Override
-        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+        public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {}
 
         @Override
-        public void onActivityStarted(Activity activity) {}
+        public void onActivityStarted(@NonNull Activity activity) {}
 
         @Override
-        public void onActivityResumed(Activity activity) {}
+        public void onActivityResumed(@NonNull Activity activity) {}
 
         @Override
-        public void onActivityPaused(Activity activity) {}
+        public void onActivityPaused(@NonNull Activity activity) {}
 
         @Override
-        public void onActivityStopped(Activity activity) {}
+        public void onActivityStopped(@NonNull Activity activity) {}
 
         @Override
-        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+        public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {}
 
         @Override
-        public void onActivityDestroyed(Activity activity) {}
+        public void onActivityDestroyed(@NonNull Activity activity) {}
     }
 
 
