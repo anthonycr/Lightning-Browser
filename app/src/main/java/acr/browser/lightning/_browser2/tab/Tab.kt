6ce04eda5a9f507683bdc93bd1007d3e2bb3fb47
Anthony Restaino <anthonyrestaino11@gmail.com>
2020-09-14 23:25:24 -0400

    Extract TabModel

--- app/src/main/java/acr/browser/lightning/_browser2/tab/Tab.kt
+++ app/src/main/java/acr/browser/lightning/_browser2/tab/Tab.kt
@@ -1,8 +1,5 @@
 package acr.browser.lightning._browser2.tab
 
-import acr.browser.lightning.ssl.SslState
-import io.reactivex.Observable
-
 /**
  * Created by anthonycr on 9/11/20.
  */
@@ -12,58 +9,3 @@ data class Tab(
     val title: String,
     val isSelected: Boolean
 )
-
-/*
-probably needs to be an interface
- */
-
-
-interface TabModel {
-
-    val id: Int
-
-    // Navigation
-
-    fun loadUrl(url: String)
-
-    fun goBack()
-
-    fun canGoBack(): Boolean
-
-    fun canGoBackChanges(): Observable<Boolean>
-
-    fun goForward()
-
-    fun canGoForward(): Boolean
-
-    fun canGoForwardChanges(): Observable<Boolean>
-
-    fun reload()
-
-    fun stopLoading()
-
-    // Data
-
-    val url: String
-
-    fun urlChanges(): Observable<String>
-
-    val title: String
-
-    fun titleChanges(): Observable<String>
-
-    val sslState: SslState
-
-    fun sslChanges(): Observable<SslState>
-
-    val loadingProgress: Int
-
-    fun loadingProgress(): Observable<Int>
-
-    // Lifecycle
-
-    var isForeground: Boolean
-
-    fun destroy()
-
-}
---
+++ app/src/main/java/acr/browser/lightning/_browser2/tab/TabModel.kt
@@ -0,0 +1,54 @@
+package acr.browser.lightning._browser2.tab
+
+import acr.browser.lightning.ssl.SslState
+import io.reactivex.Observable
+
+interface TabModel {
+
+    val id: Int
+
+    // Navigation
+
+    fun loadUrl(url: String)
+
+    fun goBack()
+
+    fun canGoBack(): Boolean
+
+    fun canGoBackChanges(): Observable<Boolean>
+
+    fun goForward()
+
+    fun canGoForward(): Boolean
+
+    fun canGoForwardChanges(): Observable<Boolean>
+
+    fun reload()
+
+    fun stopLoading()
+
+    // Data
+
+    val url: String
+
+    fun urlChanges(): Observable<String>
+
+    val title: String
+
+    fun titleChanges(): Observable<String>
+
+    val sslState: SslState
+
+    fun sslChanges(): Observable<SslState>
+
+    val loadingProgress: Int
+
+    fun loadingProgress(): Observable<Int>
+
+    // Lifecycle
+
+    var isForeground: Boolean
+
+    fun destroy()
+
+}
