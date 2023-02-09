b206d7491ad942801ee7564aed60efb6e2f8caa5
Anthony Restaino <anthonyrestaino11@gmail.com>
2023-02-09 01:21:20 -0500

    Updating gradle version and AGP plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
     }
