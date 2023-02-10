4fc900778cb753bc354693b57f22610464ce2bf1
Anthony Restaino <anthonyrestaino11@gmail.com>
2023-02-09 23:00:18 -0500

    Enable configuration cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 buildscript {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
         jcenter()
@@ -7,7 +8,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.45.0")
     }
 
     extra.apply {
