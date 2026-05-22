97beb1dd3c2db5e30cb099e8269a80b002d9a76d
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-05-22 19:37:37 -0400

    Convert FileUtils to Kotlin

--- app/src/main/java/acr/browser/lightning/utils/FileUtils.kt
+++ app/src/main/java/acr/browser/lightning/utils/FileUtils.kt
@@ -1,75 +1,68 @@
-package acr.browser.lightning.utils;
+package acr.browser.lightning.utils
 
-import android.app.Application;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcel;
-import android.util.Log;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import io.reactivex.rxjava3.core.Completable;
+import android.app.Application
+import android.os.Bundle
+import android.os.Environment
+import android.os.Parcel
+import android.util.Log
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.functions.Action
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.PrintStream
 
 /**
- * A utility class containing helpful methods
- * pertaining to file storage.
+ * A utility class containing helpful methods pertaining to file storage.
  */
-public final class FileUtils {
+object FileUtils {
+    private const val TAG = "FileUtils"
 
-    private static final String TAG = "FileUtils";
-
-    public static final String DEFAULT_DOWNLOAD_PATH =
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
-
-    private FileUtils() {}
+    val DEFAULT_DOWNLOAD_PATH: String =
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path
 
     /**
      * Writes a bundle to persistent storage in the files directory
      * using the specified file name. This method is a blocking
      * operation.
-     *
+     * 
      * @param app    the application needed to obtain the file directory.
      * @param bundle the bundle to store in persistent storage.
      * @param name   the name of the file to store the bundle in.
      */
-    public static Completable writeBundleToStorage(final @NonNull Application app, final Bundle bundle, final @NonNull String name) {
-        return Completable.fromAction(() -> {
-            File outputFile = new File(app.getFilesDir(), name);
-            FileOutputStream outputStream = null;
+    fun writeBundleToStorage(app: Application, bundle: Bundle?, name: String): Completable {
+        return Completable.fromAction(Action {
+            val outputFile = File(app.filesDir, name)
+            var outputStream: FileOutputStream? = null
             try {
-                outputStream = new FileOutputStream(outputFile);
-                Parcel parcel = Parcel.obtain();
-                parcel.writeBundle(bundle);
-                outputStream.write(parcel.marshall());
-                outputStream.flush();
-                parcel.recycle();
-            } catch (IOException e) {
-                Log.e(TAG, "Unable to write bundle to storage");
+                outputStream = FileOutputStream(outputFile)
+                val parcel = Parcel.obtain()
+                parcel.writeBundle(bundle)
+                outputStream.write(parcel.marshall())
+                outputStream.flush()
+                parcel.recycle()
+            } catch (e: IOException) {
+                Log.e(TAG, "Unable to write bundle to storage")
             } finally {
-                Utils.close(outputStream);
+                Utils.close(outputStream)
             }
-        });
+        })
     }
 
     /**
      * Use this method to delete the bundle with the specified name.
      * This is a blocking call and should be used within a worker
      * thread unless immediate deletion is necessary.
-     *
+     * 
      * @param app  the application object needed to get the file.
      * @param name the name of the file.
      */
-    public static void deleteBundleInStorage(final @NonNull Application app, final @NonNull String name) {
-        File outputFile = new File(app.getFilesDir(), name);
+    fun deleteBundleInStorage(app: Application, name: String) {
+        val outputFile = File(app.filesDir, name)
         if (outputFile.exists()) {
-            outputFile.delete();
+            outputFile.delete()
         }
     }
 
@@ -77,152 +70,154 @@ public final class FileUtils {
      * Reads a bundle from the file with the specified
      * name in the peristent storage files directory.
      * This method is a blocking operation.
-     *
+     * 
      * @param app  the application needed to obtain the files directory.
      * @param name the name of the file to read from.
      * @return a valid Bundle loaded using the system class loader
      * or null if the method was unable to read the Bundle from storage.
      */
-    @Nullable
-    public static Bundle readBundleFromStorage(@NonNull Application app, @NonNull String name) {
-        File inputFile = new File(app.getFilesDir(), name);
-        FileInputStream inputStream = null;
+    fun readBundleFromStorage(app: Application, name: String): Bundle? {
+        val inputFile = File(app.filesDir, name)
+        var inputStream: FileInputStream? = null
         try {
-            inputStream = new FileInputStream(inputFile);
-            Parcel parcel = Parcel.obtain();
-            byte[] data = new byte[(int) inputStream.getChannel().size()];
+            inputStream = FileInputStream(inputFile)
+            val parcel = Parcel.obtain()
+            val data = ByteArray(inputStream.channel.size().toInt())
 
-            //noinspection ResultOfMethodCallIgnored
-            inputStream.read(data, 0, data.length);
-            parcel.unmarshall(data, 0, data.length);
-            parcel.setDataPosition(0);
-            Bundle out = parcel.readBundle(ClassLoader.getSystemClassLoader());
-            out.putAll(out);
-            parcel.recycle();
-            return out;
-        } catch (FileNotFoundException e) {
-            Log.e(TAG, "Unable to read bundle from storage");
-        } catch (IOException e) {
-            Log.e(TAG, "Unable to read bundle from storage", e);
+            inputStream.read(data, 0, data.size)
+            parcel.unmarshall(data, 0, data.size)
+            parcel.setDataPosition(0)
+            val out = parcel.readBundle(ClassLoader.getSystemClassLoader())
+            out!!.putAll(out)
+            parcel.recycle()
+            return out
+        } catch (e: FileNotFoundException) {
+            Log.e(TAG, "Unable to read bundle from storage")
+        } catch (e: IOException) {
+            Log.e(TAG, "Unable to read bundle from storage", e)
         } finally {
-            Utils.close(inputStream);
+            Utils.close(inputStream)
         }
-        return null;
+        return null
     }
 
     /**
      * Writes a stacktrace to the downloads folder with
      * the following filename: [EXCEPTION]_[TIME OF CRASH IN MILLIS].txt
-     *
+     * 
      * @param throwable the Throwable to log to external storage
      */
-    public static void writeCrashToStorage(@NonNull Throwable throwable) {
-        String fileName = throwable.getClass().getSimpleName() + '_' + System.currentTimeMillis() + ".txt";
-        File outputFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+    fun writeCrashToStorage(throwable: Throwable) {
+        val fileName =
+            throwable.javaClass.simpleName + '_' + System.currentTimeMillis() + ".txt"
+        val outputFile = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            fileName
+        )
 
-        FileOutputStream outputStream = null;
+        var outputStream: FileOutputStream? = null
         try {
-            outputStream = new FileOutputStream(outputFile);
-            throwable.printStackTrace(new PrintStream(outputStream));
-            outputStream.flush();
-        } catch (IOException e) {
-            Log.e(TAG, "Unable to write bundle to storage");
+            outputStream = FileOutputStream(outputFile)
+            throwable.printStackTrace(PrintStream(outputStream))
+            outputStream.flush()
+        } catch (e: IOException) {
+            Log.e(TAG, "Unable to write bundle to storage")
         } finally {
-            Utils.close(outputStream);
+            Utils.close(outputStream)
         }
     }
 
     /**
      * Converts megabytes to bytes.
-     *
+     * 
      * @param megaBytes the number of megabytes.
      * @return the converted bytes.
      */
-    public static long megabytesToBytes(long megaBytes) {
-        return megaBytes * 1024 * 1024;
+    fun megabytesToBytes(megaBytes: Long): Long {
+        return megaBytes * 1024 * 1024
     }
 
     /**
      * Determine whether there is write access in the given directory. Returns false if a
      * file cannot be created in the directory or if the directory does not exist.
-     *
+     * 
      * @param directory the directory to check for write access
      * @return returns true if the directory can be written to or is in a directory that can
      * be written to. false if there is no write access.
      */
-    public static boolean isWriteAccessAvailable(@Nullable String directory) {
-        if (directory == null || directory.isEmpty()) {
-            return false;
+    fun isWriteAccessAvailable(directory: String?): Boolean {
+        if (directory.isNullOrEmpty()) {
+            return false
         }
 
-        final String sFileName = "test";
-        final String sFileExtension = ".txt";
-        String dir = addNecessarySlashes(directory);
-        dir = getFirstRealParentDirectory(dir);
-        File file = new File(dir + sFileName + sFileExtension);
-        for (int n = 0; n < 100; n++) {
+        val sFileName = "test"
+        val sFileExtension = ".txt"
+        var dir = addNecessarySlashes(directory)
+        dir = getFirstRealParentDirectory(dir)
+        var file = File(dir + sFileName + sFileExtension)
+        for (n in 0..99) {
             if (!file.exists()) {
                 try {
                     if (file.createNewFile()) {
-                        //noinspection ResultOfMethodCallIgnored
-                        file.delete();
+                        file.delete()
                     }
-                    return true;
-                } catch (IOException ignored) {
-                    return false;
+                    return true
+                } catch (ignored: IOException) {
+                    return false
                 }
             } else {
-                file = new File(dir + sFileName + '-' + n + sFileExtension);
+                file = File("$dir$sFileName-$n$sFileExtension")
             }
         }
-        return file.canWrite();
+        return file.canWrite()
     }
 
     /**
      * Returns the first parent directory of a directory that exists. This is useful
      * for subdirectories that do not exist but their parents do.
-     *
+     * 
      * @param directory the directory to find the first existent parent
      * @return the first existent parent
      */
-    @NonNull
-    private static String getFirstRealParentDirectory(@Nullable String directory) {
+    private fun getFirstRealParentDirectory(directory: String?): String {
+        var directory = directory
         while (true) {
-            if (directory == null || directory.isEmpty()) {
-                return "/";
+            if (directory.isNullOrEmpty()) {
+                return "/"
             }
-            directory = addNecessarySlashes(directory);
-            File file = new File(directory);
-            if (!file.isDirectory()) {
-                int indexSlash = directory.lastIndexOf('/');
+            directory = addNecessarySlashes(directory)
+            val file = File(directory)
+            if (!file.isDirectory) {
+                val indexSlash = directory.lastIndexOf('/')
                 if (indexSlash > 0) {
-                    String parent = directory.substring(0, indexSlash);
-                    int previousIndex = parent.lastIndexOf('/');
+                    val parent = directory.substring(0, indexSlash)
+                    val previousIndex = parent.lastIndexOf('/')
                     if (previousIndex > 0) {
-                        directory = parent.substring(0, previousIndex);
+                        directory = parent.substring(0, previousIndex)
                     } else {
-                        return "/";
+                        return "/"
                     }
                 } else {
-                    return "/";
+                    return "/"
                 }
             } else {
-                return directory;
+                return directory
             }
         }
     }
 
-    @NonNull
-    public static String addNecessarySlashes(@Nullable String originalPath) {
-        if (originalPath == null || originalPath.length() == 0) {
-            return "/";
+    @JvmStatic
+    fun addNecessarySlashes(originalPath: String?): String {
+        var originalPath = originalPath
+        if (originalPath.isNullOrEmpty()) {
+            return "/"
         }
-        if (originalPath.charAt(originalPath.length() - 1) != '/') {
-            originalPath = originalPath + '/';
+        if (originalPath[originalPath.length - 1] != '/') {
+            originalPath = "$originalPath/"
         }
-        if (originalPath.charAt(0) != '/') {
-            originalPath = '/' + originalPath;
+        if (originalPath[0] != '/') {
+            originalPath = "/$originalPath"
         }
-        return originalPath;
+        return originalPath
     }
 }
