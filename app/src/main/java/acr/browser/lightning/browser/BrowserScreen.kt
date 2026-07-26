db57b997461f4ce379a049bf5bc88318a0e8048b
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-07-26 08:59:32 -0400

    Add missing import

--- app/src/main/java/acr/browser/lightning/browser/BrowserScreen.kt
+++ app/src/main/java/acr/browser/lightning/browser/BrowserScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
