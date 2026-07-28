cb7f30323d596d2b0eb2759b1e75df7d9712f309
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-07-27 22:49:42 -0400

    Remove unnecessary metadata buildscript dependency now that dagp fixed the linkage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-buildscript {
-    dependencies {
-        // Force metadata to match Kotlin version
-        // See https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1661
-        classpath("org.jetbrains.kotlin:kotlin-metadata-jvm:2.4.10")
-    }
-}
-
 plugins {
     id("com.android.application") version "9.3.1" apply false
     id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
