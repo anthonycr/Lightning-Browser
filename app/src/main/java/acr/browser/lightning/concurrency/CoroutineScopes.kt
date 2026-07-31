611cf00abc1e2cf96607da70d7b278f4e6e66498
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-07-31 08:45:49 -0400

    Add documentation to scopes

--- app/src/main/java/acr/browser/lightning/concurrency/CoroutineScopes.kt
+++ app/src/main/java/acr/browser/lightning/concurrency/CoroutineScopes.kt
@@ -2,8 +2,19 @@ package acr.browser.lightning.concurrency
 
 import kotlinx.coroutines.CoroutineScope
 
+/**
+ * The coroutine scope that lives for the duration of the entire app process.
+ */
 class AppCoroutineScope(coroutineScope: CoroutineScope) : CoroutineScope by coroutineScope
 
+/**
+ * The coroutine scope that lives for the duration of the browser. This is usually roughly the same
+ * scope as [AppCoroutineScope], but it's possible for it to be shorter.
+ */
 class BrowserCoroutineScope(coroutineScope: CoroutineScope) : CoroutineScope by coroutineScope
 
+/**
+ * The coroutine scope that lives for the duration of a tab. All work associated to a specific tab
+ * should be scheduled using it.
+ */
 class TabCoroutineScope(coroutineScope: CoroutineScope) : CoroutineScope by coroutineScope
