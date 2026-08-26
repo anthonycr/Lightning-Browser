9b736ae36cbe462e46b72095e0efb5598486e194
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-08-26 08:05:31 -0400

    Replace deprecated function

--- app/src/test/java/acr/browser/lightning/extensions/CloseableExtensionsTest.kt
+++ app/src/test/java/acr/browser/lightning/extensions/CloseableExtensionsTest.kt
@@ -4,9 +4,8 @@ import acr.browser.lightning.SDK_VERSION
 import acr.browser.lightning.TestApplication
 import com.anthonycr.mockingbird.core.fake
 import com.anthonycr.mockingbird.core.verify
-import org.junit.Rule
+import org.junit.Assert.assertThrows
 import org.junit.Test
-import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -22,10 +21,6 @@ class CloseableExtensionsTest {
 
     private val closeable = fake<Closeable>()
 
-    @Rule
-    @JvmField
-    val exception: ExpectedException = ExpectedException.none()
-
     @Test
     fun `safeUse swallows exception`() {
         // Exception swallowed
@@ -37,11 +32,10 @@ class CloseableExtensionsTest {
             closeable.close()
         }
 
-        exception.expect(Exception::class.java)
-        exception.expectMessage("test exception")
-
-        closeable.use {
-            throw Exception("test exception")
+        assertThrows("test exception", Exception::class.java) {
+            closeable.use {
+                throw Exception("test exception")
+            }
         }
     }
 }
