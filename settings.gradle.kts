601d52dfc1720c18b8fc9bd942aceae2ae460621
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-09-02 21:20:57 -0400

    Query plugin portal first

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,8 +9,8 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
     }
 }
 
