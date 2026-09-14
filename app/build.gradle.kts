a091e500f03f7376b38bf8ba34bad89f29fec41f
Anthony Restaino <anthonyrestaino11@gmail.com>
2026-09-14 08:17:09 -0400

    Fixing robolectric tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,22 @@ android {
         abortOnError = true
     }
     namespace = "acr.browser.lightning"
+
+    testOptions {
+        unitTests.all {
+            it.jvmArgs = listOf(
+                "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                "--add-opens=java.base/java.util=ALL-UNNAMED",
+                "--add-opens=java.base/java.io=ALL-UNNAMED",
+                "--add-opens=java.base/java.net=ALL-UNNAMED",
+                "--add-opens=java.base/java.security=ALL-UNNAMED",
+                "--add-opens=java.base/java.text=ALL-UNNAMED",
+                "--add-opens=java.base/jdk.internal.access=ALL-UNNAMED",
+                "--add-opens=java.desktop/java.awt.font=ALL-UNNAMED",
+                "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+            )
+        }
+    }
 }
 
 androidComponents {
